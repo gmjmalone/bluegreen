4bce1e2c31dcada379bb32774464b7bae9f3702f
gmjmalone <40672829+gmjmalone@users.noreply.github.com>
2018-09-17 11:06:22 +0100

    Update image.php

--- image.php
+++ image.php
@@ -7,6 +7,7 @@ $green = imagecolorallocate($im, 0, 255, 0);
 $red = imagecolorallocate($im, 255, 0, 0);
 $black = imagecolorallocate($im, 0, 0, 0);
 $yellow = imagecolorallocate($im, 255, 255, 0);
+
 // Draw a filled rectangle
 //imagefilledrectangle($im, 0, 0, 199, 199, $blue);
 //imagefilledrectangle($im, 0, 0, 199, 199, $green);
